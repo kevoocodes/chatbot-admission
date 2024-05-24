07db639b0c6cc43b0294a72241d9f4b705ee6fce
kevoocodes <97kelvin1@gmail.com>
2024-05-24 05:39:20 +0300

    change query

--- app/Http/Controllers/Admin/AdminDashboardController.php
+++ app/Http/Controllers/Admin/AdminDashboardController.php
@@ -26,11 +26,37 @@ class AdminDashboardController extends Controller
         $this->data['students'] = Student::count();
 
         //return all students
-        $this->data['allstudents'] = Student::all();
-        $this->data['sumofamount'] =  Application::sum('amount');
+        //$this->data['allstudents'] = Student::all();
+        $this->data['allstudents'] = DB::table('students')
+        ->join('nactes', 'students.nacteNumber', '=', 'nactes.student_number')
+        ->join('applications', 'students.id', '=', 'applications.student_id')
+        ->join('student_academic_information', 'students.id', '=', 'student_academic_information.student_id')
+        ->select(
+            'students.fullname as student_name',
+            'students.phoneNumber',
+            'students.nidaNumber',
+            'students.nacteNumber',
+            'students.email',
+            'students.address',
+            'students.region',
+            'students.postalCode',
+            'students.dateOfBirth',
+            'students.gender',
+            'students.parent',
+            'students.parentPhonenumber',
+            'students.nationality',
+            'applications.*',
+            'student_academic_information.*'
+        )
+        ->get();
+        $this->data['sumofamount'] =  Application::sum('amount'); //return sum of people who do application
 
         //count registered users
         $this->data['users'] = User::count();
         return view('dashboards.admin.admin_dashboard', $this->data);
     }
 }
+
+ $this->data['getadmissions'] = DB::table('nactes')->select('users.*', )
+
+
